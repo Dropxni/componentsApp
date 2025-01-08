db0c3fb21b3cb109c405bd0d1e890b30b6a9b629
Dropxni <dropxni@icloud.com>
2025-01-08 13:12:01 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'componentsApp',
+  webDir: 'www'
+};
+
+export default config;
