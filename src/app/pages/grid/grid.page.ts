c5fa0bf55d68a874224baca3e2a8c59d06d8e245
Dropxni <dropxni@icloud.com>
2025-01-29 19:39:43 -0600

    grid creation and modification

--- src/app/pages/grid/grid.page.ts
+++ src/app/pages/grid/grid.page.ts
@@ -1,21 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonGrid, IonRow, IonCol,IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle} from '@ionic/angular/standalone';
+import { IonContent, IonButtons, IonGrid, IonRow, IonCol,IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonButton, IonIcon, IonFab, IonFabButton } from '@ionic/angular/standalone';
 import { HeaderComponent } from "../../components/header/header.component";
+import { AlertController } from '@ionic/angular'
+import { addIcons } from 'ionicons';
+import { alertCircleOutline, beer, rocket, heart, shieldOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.page.html',
   styleUrls: ['./grid.page.scss'],
   standalone: true,
-  imports: [IonCol, IonRow, IonCard, IonGrid, IonContent, CommonModule, FormsModule, IonCardContent, IonCardTitle, IonCardHeader, IonCardSubtitle, HeaderComponent]
+  imports: [ IonIcon, IonCol, IonButton, IonRow, IonCard, IonGrid, IonContent, CommonModule, FormsModule, IonCardContent, IonCardTitle, IonCardHeader, IonCardSubtitle, HeaderComponent]
 })
 export class GridPage implements OnInit {
 
-  constructor() { }
+  alertCtrl = inject(AlertController);
+
+  constructor() { 
+
+    addIcons({shieldOutline,heart,rocket,alertCircleOutline,beer});
+
+   }
 
   ngOnInit() {
   }
+
+  async openAlert() {
+
+    const alert = await this.alertCtrl.create({
+      header: 'AATROX:',
+      message: '"Voy a destruirlos a todos… no por venganza, sino por necesidad."'
+    });
+
+    await alert.present();
+  
+  } 
 
 }
